ea20573e9702936995f58de78ee7e644de395c9d
Mark <mma1@hwemail.com>
2023-08-29 08:47:24 -0700

    Fixed bug & wrote tester
    made 'fileName' a parameter in both methods instead of it being a preset variable. wrote a tester

--- Milo.java
+++ Milo.java
@@ -2,18 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Milo {
-    public void writeToFile(String s) throws IOException {
-        String fileName = "milo.txt";
+    public static void writeToFile(String s, String fileName) throws IOException {
+        // String fileName = "milo.txt";
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
         pw.print(s);
         pw.close();
     }
 
-    public String readFromFile() throws IOException {
-        String fileName = "milobear.txt";
+    public static String readFromFile(String fileName) throws IOException {
+        // String fileName = "milobear.txt";
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         String s = br.readLine();
         br.close();
         return s;
+    }
+    public static void main(String[] args) throws IOException {
+        // writeToFile("milo","milo.txt");
+        System.out.println(readFromFile ("milo.txt"));
     }
 }
