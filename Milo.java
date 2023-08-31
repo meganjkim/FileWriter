be085d4624aaddf6e43b8e4ff2ae7c6aa2ffad3b
meganjkim <megankim1221@gmail.com>
2023-08-31 13:16:50 -0700

    Edited methods

--- Milo.java
+++ Milo.java
@@ -2,16 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Milo {
-    public void writeToFile(String s) throws IOException {
-        String fileName = "milo.txt";
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    public void writeToFile(String s, String name) throws IOException {
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(name)));
         pw.print(s);
         pw.close();
     }
 
-    public String readFromFile() throws IOException {
-        String fileName = "milobear.txt";
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
+    public String readFromFile(String name) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(name));
         String s = br.readLine();
         br.close();
         return s;
